fea1e199d1b636375bf61a630bef3f2b044a59c8
ustcr7 <dlut200803001@163.com>
2017-06-06 00:53:12 +0800

    move

--- src/apps/gamesvr/move/move.h
+++ src/apps/gamesvr/move/move.h
@@ -1,1 +1,14 @@
- 
+#ifndef __H_GAMESVR_MOVE_H
+#define __H_GAMESVR_MOVE_H
+
+
+//client: 计算移动路径,发送给svr
+//server: svr按照移动路径,进行移动, 
+
+//服务器定期把玩家的真实位置 同步给client, 如果发现位移偏差较大, client将表现位置强制拖拽到真实位置 ?看来拖拽是客户端做的?
+
+//为什么要有对时逻辑???
+
+
+
+#endif // !__H_GAMESVR_MOVE_H
