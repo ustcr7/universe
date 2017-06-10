a7f972f6226d17df0149b519d7e1f5bc7729aa46
ustcr7 <dlut200803001@163.com>
2017-06-11 01:10:33 +0800

    客户端登录界面

---
+++ client/Assets/script/ButtonHandle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonHandle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Click()
+    {
+        Debug.Log("登录成功");
+    }
+}
