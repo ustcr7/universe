70efbb4ef65dd332db50dc4ab21f2f19870c4702
ustcr7 <dlut200803001@163.com>
2017-06-16 00:54:25 +0800

    battle system

---
+++ src/apps/gamesvr/battle/battle.h
@@ -0,0 +1,11 @@
+#ifndef  __UNIVERSE_BATTLE_H__
+#define  __UNIVERSE_BATTLE_H__
+
+//玩家状态 :alive, dead
+//玩家属性 : hp, mp, damage, defend   (血量,魔法,伤害值,防御值)
+//技能列表 : [spellid, last_cast_time]    资源:[spellid, effect_type:(伤害/治疗), effect_value, 消耗mp值]
+
+struct 
+
+
+#endif // ! __UNIVERSE_BATTLE_H__
